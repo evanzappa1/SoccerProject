db32bc1d6735acdb1f147954adc780a37f029cc5
evanzappa1 <113848739+evanzappa1@users.noreply.github.com>
2023-07-17 16:27:00 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -42,6 +42,14 @@ const howToButton = document.getElementById('how-to-button');
 howToButton.addEventListener('click', () => {
   openPopup();
 });
+const statistics = document.getElementById("stats");
+statistics.addEventListener('click', () => {
+  openStatistics();
+})
+const closeStatsButton = document.getElementById('closeStatisticsButton');
+closeStatsButton.addEventListener('click', () => {
+  closeStatistics();
+});
 const closeSearchButton = document.getElementById('closeSearchButton');
 closeSearchButton.addEventListener('click', () => {
   closeSearch();
@@ -63,8 +71,9 @@ function closePopup() {
   document.getElementById("popup").style.display = "none";
 }
 function searchName(playerName, id) {
+  /*
   var params = {
-    TableName: "playerData",
+    TableName: "allPlayers",
     ProjectionExpression: "PlayerName, TeamList",
     KeyConditionExpression: "PlayerName = :name",
     ExpressionAttributeValues: {
@@ -83,6 +92,7 @@ function searchName(playerName, id) {
     if (err) {
       console.error("Error fetching players:", err);
     } else {
+
       playerNames =  data.Items.map(item => item.PlayerName);
       playerNames = playerNames.join(',')
       teamList =  data.Items.map(item => item.TeamList);
@@ -91,35 +101,35 @@ function searchName(playerName, id) {
         if (id == "cell1") {
           team1 = document.getElementById("row-1").querySelector('img').getAttribute('alt')
           team2 = document.getElementById("column-1").querySelector('img').getAttribute('alt')
-          if (teamList.includes(team1) || teamList.includes(team2)) {
+          if (teamList.includes(team1) && teamList.includes(team2)) {
             correctGuess(playerName, document.getElementById("cell1"))
           }
         }
         if (id == "cell2") {
           team1 = document.getElementById("row-1").querySelector('img').getAttribute('alt')
           team2 = document.getElementById("column-2").querySelector('img').getAttribute('alt')
-          if (teamList.includes(team1) || teamList.includes(team2)) {
+          if (teamList.includes(team1) && teamList.includes(team2)) {
             correctGuess(playerName, document.getElementById("cell2"))
           }
         }
         if (id == "cell3") {
           team1 = document.getElementById("row-1").querySelector('img').getAttribute('alt')
           team2 = document.getElementById("column-3").querySelector('img').getAttribute('alt')
-          if (teamList.includes(team1) || teamList.includes(team2)) {
+          if (teamList.includes(team1) && teamList.includes(team2)) {
             correctGuess(playerName, document.getElementById("cell3"))
           }
         }
         if (id == "cell4") {
           team1 = document.getElementById("row-2").querySelector('img').getAttribute('alt')
           team2 = document.getElementById("column-1").querySelector('img').getAttribute('alt')
-          if (teamList.includes(team1) || teamList.includes(team2)) {
+          if (teamList.includes(team1) && teamList.includes(team2)) {
             correctGuess(playerName, document.getElementById("cell4"))
           }
         }
         if (id == "cell5") {
           team1 = document.getElementById("row-2").querySelector('img').getAttribute('alt')
           team2 = document.getElementById("column-2").querySelector('img').getAttribute('alt')
-          if (teamList.includes(team1) || teamList.includes(team2)) {
+          if (teamList.includes(team1) && teamList.includes(team2)) {
             correctGuess(playerName, document.getElementById("cell5"))
           }
         }
@@ -133,25 +143,101 @@ function searchName(playerName, id) {
         if (id == "cell7") {
           team1 = document.getElementById("row-3").querySelector('img').getAttribute('alt')
           team2 = document.getElementById("column-1").querySelector('img').getAttribute('alt')
-          if (teamList.includes(team1) || teamList.includes(team2)) {
+          if (teamList.includes(team1) && teamList.includes(team2)) {
             correctGuess(playerName, document.getElementById("cell7"))
           }
         }
         if (id == "cell8") {
           team1 = document.getElementById("row-3").querySelector('img').getAttribute('alt')
           team2 = document.getElementById("column-2").querySelector('img').getAttribute('alt')
-          if (teamList.includes(team1) || teamList.includes(team2)) {
+          if (teamList.includes(team1) && teamList.includes(team2)) {
             correctGuess(playerName, document.getElementById("cell8"))
           }
         }
         if (id == "cell9") {
           team1 = document.getElementById("row-3").querySelector('img').getAttribute('alt')
           team2 = document.getElementById("column-3").querySelector('img').getAttribute('alt')
-          if (teamList.includes(team1) || teamList.includes(team2)) {
+          if (teamList.includes(team1) && teamList.includes(team2)) {
             correctGuess(playerName, document.getElementById("cell9"))
           }
         }
     }
+    })
+
+   */
+  fetch('all_prem.json')
+    .then(response => response.json())
+    .then(data => {
+      // The data variable now contains the parsed JSON data
+      playerData = data.find(player => player.PlayerName === playerName);
+      const teamList = playerData.TeamList;
+      const nationalities = playerData.Nationalities;
+      const positions = playerData.Positions;
+      const YearsActive = playerData.TeamList;
+      console.log(playerData)
+      if (id == "cell1") {
+        team1 = document.getElementById("row-1").querySelector('img').getAttribute('alt')
+        team2 = document.getElementById("column-1").querySelector('img').getAttribute('alt')
+        if (teamList.includes(team1) && teamList.includes(team2)) {
+          correctGuess(playerName, document.getElementById("cell1"))
+        }
+      }
+      if (id == "cell2") {
+        team1 = document.getElementById("row-1").querySelector('img').getAttribute('alt')
+        team2 = document.getElementById("column-2").querySelector('img').getAttribute('alt')
+        if (teamList.includes(team1) && teamList.includes(team2)) {
+          correctGuess(playerName, document.getElementById("cell2"))
+        }
+      }
+      if (id == "cell3") {
+        team1 = document.getElementById("row-1").querySelector('img').getAttribute('alt')
+        team2 = document.getElementById("column-3").querySelector('img').getAttribute('alt')
+        if (teamList.includes(team1) && teamList.includes(team2)) {
+          correctGuess(playerName, document.getElementById("cell3"))
+        }
+      }
+      if (id == "cell4") {
+        team1 = document.getElementById("row-2").querySelector('img').getAttribute('alt')
+        team2 = document.getElementById("column-1").querySelector('img').getAttribute('alt')
+        if (teamList.includes(team1) && teamList.includes(team2)) {
+          correctGuess(playerName, document.getElementById("cell4"))
+        }
+      }
+      if (id == "cell5") {
+        team1 = document.getElementById("row-2").querySelector('img').getAttribute('alt')
+        team2 = document.getElementById("column-2").querySelector('img').getAttribute('alt')
+        if (teamList.includes(team1) && teamList.includes(team2)) {
+          correctGuess(playerName, document.getElementById("cell5"))
+        }
+      }
+      if (id == "cell6") {
+        team1 = document.getElementById("row-2").querySelector('img').getAttribute('alt')
+        team2 = document.getElementById("column-3").querySelector('img').getAttribute('alt')
+        if (teamList.includes(team1) || teamList.includes(team2)) {
+          correctGuess(playerName, document.getElementById("cell6"))
+        }
+      }
+      if (id == "cell7") {
+        team1 = document.getElementById("row-3").querySelector('img').getAttribute('alt')
+        team2 = document.getElementById("column-1").querySelector('img').getAttribute('alt')
+        if (teamList.includes(team1) && teamList.includes(team2)) {
+          correctGuess(playerName, document.getElementById("cell7"))
+        }
+      }
+      if (id == "cell8") {
+        team1 = document.getElementById("row-3").querySelector('img').getAttribute('alt')
+        team2 = document.getElementById("column-2").querySelector('img').getAttribute('alt')
+        if (teamList.includes(team1) && teamList.includes(team2)) {
+          correctGuess(playerName, document.getElementById("cell8"))
+        }
+      }
+      if (id == "cell9") {
+        team1 = document.getElementById("row-3").querySelector('img').getAttribute('alt')
+        team2 = document.getElementById("column-3").querySelector('img').getAttribute('alt')
+        if (teamList.includes(team1) && teamList.includes(team2)) {
+          correctGuess(playerName, document.getElementById("cell9"))
+        }
+      }
     })
   updateGuesses()
   closeSearch()
@@ -160,6 +246,7 @@ function searchName(playerName, id) {
 function correctGuess(playerName,id) {
   id.innerHTML = playerName
   id.style.backgroundColor = "green"
+  console.log(playerName,id)
 }
 
 
@@ -174,11 +261,12 @@ let closeSearch = function () {
 }
 
 function handleAutofill(player_id) {
+  /*
   const wrapper = document.querySelector(".popup-container");
   const searchInp = wrapper.querySelector("input");
   const options = wrapper.querySelector(".options");
   var params = {
-    TableName: "playerData",
+    TableName: "allPlayers",
     ProjectionExpression: "PlayerName"
   };
   var AWS = require('aws-sdk');
@@ -207,6 +295,29 @@ function handleAutofill(player_id) {
       });
     }
     })
+
+   */
+  const wrapper = document.querySelector(".popup-container");
+  const searchInp = wrapper.querySelector("input");
+  const options = wrapper.querySelector(".options");
+  fetch('all_prem.json')
+    .then(response => response.json())
+    .then(data => {
+      // The data variable now contains the parsed JSON data
+      const playerNames = data.map(player => player.PlayerName);
+      let arr = [];
+      let searchedVal = searchInp.value.toLowerCase();
+      arr = playerNames
+        .filter(data => data.toLowerCase().startsWith(searchedVal))
+        .map(data => `<li>${data}</li>`)
+        .join("");
+      options.innerHTML = arr ? arr : '<p>Player not found!</p>';
+      const liItems = options.querySelectorAll("li");
+      liItems.forEach((li) => {
+        li.addEventListener("click", () => searchName(li.innerHTML, player_id));
+      });
+    })
+
 }
 function openSearch(id) {
   document.getElementById("search-popup").style.display = "block";
@@ -217,3 +328,12 @@ function openSearch(id) {
   options.innerHTML = ""; // Clear the options list
   searchInp.addEventListener("keyup", () => handleAutofill(id)); // Add the keyup event listener
 }
+
+function openStatistics() {
+  console.log("YES")
+  document.getElementById("statistics-popup").style.display = "block";
+};
+function closeStatistics() {
+  document.getElementById("statistics-popup").style.display = "none";
+}
+
